27365a6d43fcd6797b18b61201673a92f925dc91
Naveen-saka <sakanaveen2006@gmail.com>
2026-08-23 10:15:14 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (54.88%) - LeetSync

---
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        unordered_set<int> st;
+        
+        for (int p : nums) {
+            int x = p;
+            for (int i = 2; i * i <= x; i++) {
+                if (x % i == 0) {
+                    st.insert(i);
+                    while (x % i == 0) x /= i;
+                }
+            }
+            if (x > 1) st.insert(x);
+        }
+        
+        return st.size();
+    }
+};
