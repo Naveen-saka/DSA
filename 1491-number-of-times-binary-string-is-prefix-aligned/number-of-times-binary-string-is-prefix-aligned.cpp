be975a36c196a171af051c5d0ab1d06bb0f54f54
Naveen-saka <sakanaveen2006@gmail.com>
2026-08-24 15:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (98.72%) - LeetSync

--- 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1491-number-of-times-binary-string-is-prefix-aligned/number-of-times-binary-string-is-prefix-aligned.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int numTimesAllBlue(vector<int>& flips) {
-        multiset<int>ms;
+        int maxi = 0;
         int ans = 0;
         for(int i=0;i<flips.size();i++)
         {
-            ms.insert(flips[i]);
-            if(*ms.rbegin() == i+1) ans++;
+            maxi = max(maxi,flips[i]);
+            if(maxi == i+1) ans++;
         }
         return ans;
     }
