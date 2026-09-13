0a7ba1eaab1e7c86adb9b7a345196e8410f8995e
Naveen-saka <sakanaveen2006@gmail.com>
2026-09-13 13:49:42 +0530

    Time: 727 ms (5.27%) | Memory: 378.8 MB (5.16%) - LeetSync

--- 4328-count-shadow-pairs-i/count-shadow-pairs-i.cpp
+++ 4328-count-shadow-pairs-i/count-shadow-pairs-i.cpp
@@ -13,19 +13,18 @@ public:
             st.push(i);
         }
         // for(int i:nm) cout<<i<<" ";
-        cout<<"\n";
         map<int,vector<int>>mp;
         for(int i=0;i<v.size();i++) mp[v[i]].push_back(i);
         for(int i=0;i<n;i++)
         {
-            int cur = nm[i] - i - 1;
+            int cur = nm[i] - i;
             // if(nm[i] == n) cur--;
             auto &p = mp[v[i]];
             auto r = lower_bound(p.begin(),p.end(),nm[i]) - p.begin();
             // if(r!=0) r--;
             auto l = upper_bound(p.begin(),p.end(),i) - p.begin();
             // cout<<cur-(r-l+1)<<"\n";
-            ans+=cur-(r-l);
+            ans+=cur-(r-l+1);
         }
         return ans;
     }
