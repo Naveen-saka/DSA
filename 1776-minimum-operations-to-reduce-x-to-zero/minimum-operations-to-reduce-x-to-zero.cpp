594ac1f124a8b5d537a6d5be03223c94e321ce28
Naveen-saka <sakanaveen2006@gmail.com>
2026-09-23 17:17:44 +0530

    Time: 52 ms (21.88%) | Memory: 111.6 MB (22.16%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        auto &v = nums;
+        int n = v.size();
+        vector<int>pre(n,0),suf(n,0);
+        pre[0] = v[0];
+        for(int i=1;i<n;i++) pre[i] = pre[i-1] + v[i];
+        suf.back() = v.back();
+        for(int i=n-2;i>=0;i--) suf[i] = suf[i+1] + v[i];
+        int ans = INT_MAX;
+        int sum = 0;
+        auto it = binary_search(pre.begin(),pre.end(),x);
+        if(it)
+        {
+            int idx = lower_bound(pre.begin(),pre.end(),x) - pre.begin();
+            ans = min(ans,idx + 1);
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            sum+=v[i];
+            if(sum == x) ans = min(ans,n-i);
+            else if(sum > x) break;
+            else
+            {
+                auto it = binary_search(pre.begin(),pre.begin()+i,x - sum);
+                if(it)
+                {
+                    int idx = lower_bound(pre.begin(),pre.begin()+i,x - sum) - pre.begin();
+                    ans = min(ans,n - i + idx + 1);
+                } 
+            }
+        }
+        if(ans == INT_MAX) return -1;
+        return ans;
+    }
+};
